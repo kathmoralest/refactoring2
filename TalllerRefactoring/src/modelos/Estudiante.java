b6d44c99367c7c84445e347b787fa049751e6ef3
Katherine Morales Tierra <kymorale@espol.edu.ec>
2021-08-12 22:56:22 -0500

    Inappropiate Intimancy

--- TalllerRefactoring/src/modelos/Estudiante.java
+++ TalllerRefactoring/src/modelos/Estudiante.java
@@ -3,14 +3,14 @@ package modelos;
 import java.util.ArrayList;
 
 public class Estudiante{
-    public String matricula;
-    public String nombre;
-    public String apellido;
-    public String facultad;
-    public int edad;
-    public String direccion;
-    public String telefono;
-    public ArrayList<Paralelo> paralelos;
+    private String matricula;
+    private String nombre;
+    private String apellido;
+    private String facultad;
+    private int edad;
+    private String direccion;
+    private String telefono;
+    private ArrayList<Paralelo> paralelos;
     
     public String getMatricula() {
         return matricula;
