536f946da72d98c5e15bff12cdd777ff468ddb5f
Katherine Morales Tierra <kymorale@espol.edu.ec>
2021-08-12 22:12:11 -0500

    Large class, Dead code, Comments

--- TalllerRefactoring/src/modelos/Estudiante.java
+++ TalllerRefactoring/src/modelos/Estudiante.java
@@ -3,7 +3,6 @@ package modelos;
 import java.util.ArrayList;
 
 public class Estudiante{
-    //Informacion del estudiante
     public String matricula;
     public String nombre;
     public String apellido;
@@ -13,8 +12,6 @@ public class Estudiante{
     public String telefono;
     public ArrayList<Paralelo> paralelos;
     
-    //Getter y setter de Matricula
-
     public String getMatricula() {
         return matricula;
     }
@@ -22,86 +19,32 @@ public class Estudiante{
     public void setMatricula(String matricula) {
         this.matricula = matricula;
     }
-
-    //Getter y setter del Nombre
+    
     public String getNombre() {
         return nombre;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-    
-    //Getter y setter del Apellido
     public String getApellido() {
         return apellido;
     }
 
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-    
-    //Getter y setter de la Facultad
     public String getFacultad() {
         return facultad;
     }
-
-    public void setFacultad(String facultad) {
-        this.facultad = facultad;
-    }
     
-    //Getter y setter de la edad
     public int getEdad() {
         return edad;
     }
 
-    public void setEdad(int edad) {
-        this.edad = edad;
-    }
-    
-    //Getter y setter de la direccion
     public String getDireccion() {
         return direccion;
     }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-    
-    //Getter y setter del telefono
 
     public String getTelefono() {
         return telefono;
     }
 
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
+    public ArrayList<Paralelo> getParalelos() {
+        return paralelos;
     }
-    
-    //Calcula y devuelve la nota inicial contando examen, deberes, lecciones y talleres. El teorico y el practico se calcula por parcial.
-    public double CalcularNota(Paralelo p,Notas nota){
-        double notaInicial=0;
-        for(Paralelo par:paralelos){
-            if(p.equals(par)){
-                double notaTeorico=(nota.getNexamen()+nota.getNdeberes()+nota.getNlecciones())*0.80;
-                double notaPractico=(nota.getNtalleres())*0.20;
-                notaInicial=notaTeorico+notaPractico;
-            }
-        }
-        return notaInicial;
-    }
-    
-    //Calcula y devuelve la nota inicial contando examen, deberes, lecciones y talleres. Esta nota es solo el promedio de las dos calificaciones anteriores.
-    public double CalcularNotaTotal(Paralelo p){
-        double notaTotal=0;
-        for(Paralelo par:paralelos){
-            if(p.equals(par)){
-                notaTotal=(p.getMateria().getNotaInicial() + p.getMateria().getNotaFinal())/2;
-            }
-        }
-        return notaTotal;  
-    }
-}
-        
-    
-    
+}   
